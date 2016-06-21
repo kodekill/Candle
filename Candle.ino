1033daf7a7b35075c7a46464bf2f9b54018c21f8
Brian Falor <brian.falor@gmail.com>
2016-06-20 21:44:32 -0600

    Added some random functions for the flame flicker. Code is supplied for 2 LED's

--- Candle.ino
+++ Candle.ino
@@ -12,16 +12,60 @@ void setup() {
 void loop() {
   time = random(200,500); 
   Serial.println(time);
-  LED(time);
+  LED1(time);
+
+  time = random(200,500); 
+  Serial.println(time);
+  LED2(time);
 }
 
 
 
-int LED(int time) {
-  strip.setPixelColor(0, strip.Color(64,32,0));
-  strip.setPixelColor(2, strip.Color(64,32,0));
-  strip.show();
-  delay(time); 
+int LED1(int time) {
+  
+ int brightLow = random(3, 64);
+ int brightHigh = random(80, 256);
+  
+  for (int i = brightLow; i <= brightHigh; i++)
+    {
+      strip.setBrightness(i);
+      strip.setPixelColor(0, strip.Color(64,32,0));
+      strip.show();
+      delay(time/3);
+    }
+  delay(time);
 
+  for (int i = brightHigh; i >= brightLow; i--)
+    {
+      strip.setBrightness(i);
+      strip.setPixelColor(0, strip.Color(64,32,0));
+      strip.show();
+      delay(time%3);
+    }
+  delay(time);
 }
 
+int LED2(int time) {
+  
+ int brightLow = random(3, 64);
+ int brightHigh = random(80, 256);
+  
+  for (int i = brightLow; i <= brightHigh; i++)
+    {
+      strip.setBrightness(i);
+      strip.setPixelColor(2, strip.Color(64,32,0));
+      strip.show();
+      delay(time/4);
+    }
+  delay(time);
+
+  for (int i = brightHigh; i >= brightLow; i--)
+    {
+      strip.setBrightness(i);
+      strip.setPixelColor(2, strip.Color(64,32,0));
+      strip.show();
+      delay(time%3);
+    }
+  delay(time);
+}
+ //strip.clear();
