719b3d5ac6b8dce7ded1d825cb3ce5c62d5bf9be
Brian Falor <brian.falor@gmail.com>
2016-06-20 21:29:40 -0600

    first commit

---
+++ Candle.ino
@@ -0,0 +1,27 @@
+#include <Adafruit_NeoPixel.h>
+#define PIN 2
+#define NUMPIXELS 4
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+int time = 0; 
+
+void setup() {
+  strip.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  time = random(200,500); 
+  Serial.println(time);
+  LED(time);
+}
+
+
+
+int LED(int time) {
+  strip.setPixelColor(0, strip.Color(64,32,0));
+  strip.setPixelColor(2, strip.Color(64,32,0));
+  strip.show();
+  delay(time); 
+
+}
+
